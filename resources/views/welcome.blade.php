ba59fc7830c8d0a0dab4287b0503485f8a7e1165
hellrud <hellrud@gmail.com>
2024-07-08 14:37:40 +0200

    Test commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel Guestbook</title>
-
-        <!-- Fonts -->
-
-        <!-- Styles -->
-        <style>
-       
-        </style>
+        
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-light antialiased dark:bg-black dark:text-white/50">
