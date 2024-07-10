fab4f118f764759fe50a42241f0febc777baa45b
hellrud <hellrud@gmail.com>
2024-07-10 15:30:00 +0200

    added hardcoded cards as component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,9 +27,15 @@
                     <hr class="m-6">
                      <!-- Content -->
                     <main>
-                    GuestBookContentCards
+                    <div class="flex flex-wrap gap-4 items-center justify-center">
+                        <x-content-card title="blabla title" href="guestbook/1">Content in the card</x-content-card>
+                        <x-content-card title="blabla title" href="guestbook/1">Content in the card</x-content-card>
+                        <x-content-card title="blabla title" href="guestbook/1">Content in the card</x-content-card>
+                        <x-content-card title="bla bla bla bla bla bla bla bla bla bla " href="guestbook/1">Content in the card Content in the cardContent in the cardContent in the cardContent in the cardContent in the cardContent in the cardContent in the cent in the cardContent in the cardContent in the cardContent in the card</x-content-card>
+                        <x-content-card title="blabla title" href="guestbook/1">Content in the card</x-content-card>
+                        <x-content-card title="bla bla bla bla bla bla bla bla bla bla " href="guestbook/1">Content in the card Content in the cardContent in the cardContent in the cardContent in the cardContent in the cardContent in the cardContent in the cent in the cardContent in the cardContent in the cardContent in the card</x-content-card>
+                    </div> 
                     </main>
-
     </body>
 
     <script>
