c4890382a404dda7480056557ca83f644ea43f0c
hellrud <hellrud@gmail.com>
2024-07-10 14:48:57 +0200

    darkMode Switch

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,8 +1,8 @@
 <header class="flex items-center py-4">
                         @if (Route::has('login'))
-                            <nav class="flex flex-1 justify-end gap-2 mr-2 sm:mr-10 pb-5">
+                            <nav class="flex flex-auto justify-end gap-4 mr-2 sm:mr-10 pb-5">
                                 @auth
-                                    <x-nav-link class="">
+                                    <x-nav-link class="mb-3">
                                     Hey, {{ Auth::user()->name }}
                                     </x-nav-link>        
                                     <form method="POST" action="{{ route('logout') }}">
@@ -15,16 +15,21 @@
                                         </x-nav-link>
                                     </form>
                                 @else
-                                    <x-nav-link :href="route('login')">
+                                    <x-nav-link :href="route('login')" class="mb-3">
                                         Log in
                                     </x-nav-link>
 
                                     @if (Route::has('register'))
-                                    <x-nav-link :href="route('register')">
+                                    <x-nav-link :href="route('register')" class="mb-3">
                                         Register to post
                                     </x-nav-link>
                                     @endif
                                 @endauth
+
+                                <button id="theme-toggle" type="button" class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 -mt-2">
+                    <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
+                    <svg id="theme-toggle-light-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+                </button>
                             </nav>
-                        @endif
+                        @endif              
 </header>
