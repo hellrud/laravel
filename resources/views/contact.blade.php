b7a2a343897c950da95aace1ee14bb364de7473e
hellrud <hellrud@gmail.com>
2024-09-22 22:16:15 +0200

    Placeholder

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -16,7 +16,7 @@
           <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
         </svg>
       </span>
-      <x-text-input type="text" id="name" value="{{old('name')}}" name="name" class="border-l-0 rounded-none rounded-e-md flex-1 min-w-0 w-full text-sm p-2.5" placeholder="Mike Jordan"></x-text-input>
+      <x-text-input type="text" id="name" value="{{old('name')}}" name="name" class="border-l-0 rounded-none rounded-e-md flex-1 min-w-0 w-full text-sm p-2.5" placeholder="Your Name"></x-text-input>
     </div>
 
     <label for="email" class="mt-3 block mb-2 text-sm font-medium text-gray-900 dark:text-white"><p class="@error('email') text-red-500  @enderror">Your email</p></label>
